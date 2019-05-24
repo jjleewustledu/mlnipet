1071fd567597d0aae7df16e67ccb2da63cfa87f5
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2019-05-24 15:48:29 -0500

    initial

---
+++ src/+mlnipet/ProjectData.m
@@ -0,0 +1,33 @@
+classdef ProjectData < mlpipeline.ProjectData
+	%% PROJECTDATA  
+
+	%  $Revision$
+ 	%  was created 22-May-2019 12:06:13 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlnipet/src/+mlnipet.
+ 	%% It was developed on Matlab 9.5.0.1067069 (R2018b) Update 4 for MACI64.  Copyright 2019 John Joowon Lee.
+ 	
+	properties (Dependent)
+        jsonDir	
+ 	end
+
+	methods 
+        
+        %% GET
+        
+        function g = get.jsonDir(~)
+            g = getenv('SUBJECTS_DIR');
+        end
+        
+        %%
+		  
+ 		function this = ProjectData(varargin)
+ 			%% PROJECTDATA
+ 			%  @param .
+
+ 			this = this@mlpipeline.ProjectData(varargin{:});
+ 		end
+ 	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
